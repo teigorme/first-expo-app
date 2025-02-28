ac38c823fc6aa6cee9cd60d229ecac580e4394c7
teigorme <igorteixeira354@gmail.com>
2025-02-28 19:29:34 +0100

    use stylesheet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const Index = () => {
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View style={style.container}>
+      <Text style={style.title}>Hello World!</Text>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    justifyContent: "center",
+  },
+  title: {
+    color: "red",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
 
 export default Index;
